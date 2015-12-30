97aa0a5d97203cb6802b39db91fad318a878cf4b
david kaye <dfkaye@yahoo.com>
2015-12-30 10:53:35 -0800

    first refactor::cards.json,  impls

--- js/script.js
+++ js/script.js
@@ -1,95 +1,106 @@
-var cards = [
-    {
-        "card": {
-            "name": "Tasks",
-            "role": "Student, Advisor",
-            "role": "Advisor",
-            "golive": "1",
-            "jiraNumber": "9330"
-        }
-    },
-    {
-        "card": {
-            "name": "SIR",
-            "role": "Student",
-            "role":"Advisor",
-            "golive": "3",
-            "jiraNumber": "6561"
-        }
-    },
-    {
-        "card": {
-            "name": "Aid Summary",
-            "role": "Student",
-            "golive": "4",
-            "jiraNumber": ""
-        }
-    },
-    {
-        "card": {
-            "name": "Enrollment",
-            "role": "Student",
-            "golive": "5",
-            "jiraNumber": "12204"
-        }
-    },
-    {
-        "card": {
-            "name": "Advisee Personal Summary",
-            "role": "Advisor",
-            "golive": "5",
-            "jiraNumber": "12053"
-        }
-    },
-    {
-        "card": {
-            "name": "GPA Calculator",
-            "role": "Student",
-            "golive": "6",
-            "jiraNumber": ""
-        }
+jQuery(function($){
+  
+  $.getJSON("./js/cards.json", function(data) {
+    console.warn(this)
+    data && data.length > 0 || (error([this.url, 'data was not loaded', data].join('::')));
+    
+    init(data);
+  });
+  
+  function error(msg, data) {
+    throw Error(msg, data);
+  }
+  
+  function init(cards) {
+    
+    var $role = $('#role');
+    var $golive = $('#golive');
+    var $result = $('#result');
+
+    $role.on('change', function (e) {
+      console.log('change of role')
+      console.log(e.target.selectedIndex);
+      
+      onChange();
+    });
+
+    $golive.on('change', function (e) {
+      
+      console.log('change of golive')
+      console.warn(e.target.selectedIndex);   
+      
+      onChange();
+    });
+
+    function onChange() {
+      var cards = findCards();
+      
+      showCards(cards);
     }
-  ];
+    
+    function findCards() {
+      return filter($role.find('option:selected').val(), $golive.find('option:selected').val());
+    }
 
-var $role = $('#role'),
-    $golive = $('#golive'),
-    $result = $('#result');
+    function filter(role, golive) {
+      var result = [];
 
-$role.change(function () {
-    onChange();
-});
-
-$golive.change(function () {
-    onChange();
-});
-
-function onChange() {
-    var findedCards = findCards();
-    showCards(findedCards);
-}
-
-function findCards() {
-    return filter($role.find('option:selected').val(), $golive.find('option:selected').val());
-}
-
-function filter(role, golive) {
-    var result = [];
-
-    for (var i = 0; i < cards.length; i++) {
+      for (var i = 0; i < cards.length; i++) {
         var card = cards[i].card;
+        
         if ((!role || card.role === role) && (!golive || card.golive === golive)) {
-            result.push(card);
+          result.push(card);
         }
+      }
+
+      return result;
+    }
+    
+
+    
+    // this is clearing and rewriting cards in the DOM, which is rather noisy.
+    // better to run a createCards function that writes them all once,
+    // then use filter to select cards by attributes and show only those
+    
+    function showCards(cards) {
+      
+      $result.empty();
+      
+      for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        
+        $result.append($('<div class="golive golive' + card.golive + '">' + card.name + 
+                         '<br>' + 'Go Live ' + card.golive + '<br><br>' + 
+                         '<a href="https://jira.berkeley.edu/browse/SISRP-' + 
+                         card.jiraNumber + '">' + card.jiraNumber + '</a>' + 
+                         '</div>'));
+      }
     }
 
-    return result;
-}
+    
+    
+    onChange();
+    
+  
+    $('button[data-go-live]').on('click', function updateGolive(e) {
+      
+      var index = e.target.getAttribute('data-go-live');
+      var goliveClass = '.golive';
+      var updateClass = goliveClass + index;
+      
+      $(updateClass).removeClass('inactive future').addClass('active');
 
-function showCards(cards) {
-    $result.empty();
-    for (var i = 0; i < cards.length; i++) {
-        $result.append($('<div class="golive' + cards[i].golive + '">' + cards[i].name + '<br>' + 'Go Live ' + cards[i].golive + '<br><br>' + '<a href="https://jira.berkeley.edu/browse/SISRP-' + cards[i].jiraNumber + '">' + cards[i].jiraNumber + '</a>' + '</div>'  ));
-	}
-}
-
-onChange();
+      $(goliveClass).not(updateClass).each(function(i, elm) {
+        
+        var $elm = $(elm);       
+        $elm.removeClass('active').addClass('inactive');
+        
+        var n = ('' + elm.className.match(/golive[\d]+/)).split('golive')[1];
+        if (n > index) {
+          $elm.addClass('future');
+        }
+      });
+    });
+  }
+  
+});
